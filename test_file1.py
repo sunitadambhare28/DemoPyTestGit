cc1884d0f7ac2332ac11a7c82118e55882bf3e67
Sunita Pankule <sunita.pankule@sofdel.com>
2025-05-07 09:26:02 +0530

    formated the code

--- test_file1.py
+++ test_file1.py
@@ -1,43 +1,43 @@
+class Test_1:
+    def test_sum(self):
+        a = 10
+        b = 20
+        c = a + b
+        if c == 30:
+            print("test cases passed")
+            assert True
+        else:
+            print("test cases failed")
 
-class Test_1 :
-  def test_sum(self):
-    a=10
-    b=20
-    c=a+b
-    if c==30:
-        print("test cases passed")
-        assert True
-    else:
-        print("test cases failed")
-  def test_mul(self):
-    a=10
-    b=20
-    c=a*b
-    if c==200:
-     print("test cases passed")
-     assert True
-    else:
-     print("test cases passed")
-     assert False
-  def test_sub(self):
-      a = 10
-      b = 5
-      c = a - b
-      if c == 5:
-          print("test cases passed")
-          assert True
-      else:
-          print("test cases failed")
-          assert False
+    def test_mul(self):
+        a = 10
+        b = 20
+        c = a * b
+        if c == 200:
+            print("test cases passed")
+            assert True
+        else:
+            print("test cases passed")
+            assert False
 
-  def test_sub1(self):
-      a = 10
-      b = 5
-      c = a - b
-      if c == 5:
-          print("test cases passed")
-          assert True
-      else:
-          print("test cases failed")
-          assert False
+    def test_sub(self):
+        a = 10
+        b = 5
+        c = a - b
+        if c == 5:
+            print("test cases passed")
+            assert True
+        else:
+            print("test cases failed")
+            assert False
 
+    def test_sub1(self):
+        a = 10
+        b = 5
+        c = a - b
+        if c == 5:
+            print("test cases passed")
+            assert True
+        else:
+            print("test cases failed")
+            assert False
